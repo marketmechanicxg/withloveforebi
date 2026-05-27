29c6bbd0a289e53d46034ef169c87c1a39960db0
marketmechanicxg <grizzyxixi@gmail.com>
2026-05-27 19:29:49 +0700

    Update audio-system.js

--- js/audio-system.js
+++ js/audio-system.js
@@ -14,8 +14,8 @@
 const playlist = [
   {
     id:        'song1',
-    title:     'Sampai Jadi Debu',
-    artist:    'Banda Neira',
+    title:     'Hold Me Tight',
+    artist:    'Skyline',
     file:      'music/song1.mp3',
     cover:     'assets/music-cover/song1.jpeg',
     startTime: 0,       // seconds — where playback begins
